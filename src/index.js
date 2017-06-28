70d19bea4f6c34d5434040a3c1a362d072fa77c4
Takamichi-tsutsumi <takamichi_tsutsumi@waku-2.com>
2017-06-28 18:43:18 +0900

    add redux-logger

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import logger from 'redux-logger'
 import App from './containers/App';
 import reducers from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
 
-const store = createStore(reducers, applyMiddleware(thunk));
+const store = createStore(reducers, applyMiddleware(thunk, logger));
 
 
 ReactDOM.render(
